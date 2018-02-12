d9153b85ea25751b47c1ee436695026296618c2f
morteza amzajerdi <ammorteza@yahoo.com>
2018-02-12 12:11:55 +0330

    user image slider in news page 1396/11/23

--- app/NewsSlider.php
+++ app/NewsSlider.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class NewsSlider extends Model
 {
     protected $table = 'tbl_news_slider';
+
+    public function gallery()
+    {
+        return $this->belongsTo(ImageGallery::class , 'nsGId' , 'id');
+    }
 }
